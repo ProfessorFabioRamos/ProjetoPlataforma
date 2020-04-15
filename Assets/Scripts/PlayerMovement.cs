37af5fdd7458ecc2c3ea5be90fbf8d15a1872e79
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-04-15 20:56:01 -0300

    Primeiro commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 4;
+    public float jumpForce;
+    private Rigidbody2D rig;
+    private Animator anim;
+    private SpriteRenderer spr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rig.velocity = new Vector2(movementSpeed*h, rig.velocity.y);
+    }
+}
