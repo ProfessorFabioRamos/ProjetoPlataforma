e33e6416869f4178f3ac61750a27388fe7f201c7
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-04-30 10:01:14 -0300

    29-04-20

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,20 @@ public class Enemy : MonoBehaviour
 {
     public int enemyHP = 3;
     Animator anim;
+    Rigidbody2D rig;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //transform.Translate(Vector2.left * Time.deltaTime*50);
+        rig.AddForce(new Vector2(5000,0));
     }
 
     public void TakeDamage(int damage){
