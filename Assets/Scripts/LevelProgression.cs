e953036eae6709583332235f080c18c4e3bbe743
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-05-25 19:07:55 -0300

    20-05-20

---
+++ Assets/Scripts/LevelProgression.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelProgression : MonoBehaviour
+{
+    SpriteRenderer rend;
+    public Sprite imgOpen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        if(other.tag == "Player"){
+            rend.sprite = imgOpen;
+            Invoke("LevelCompleted", 2);
+        }
+    }
+
+    void LevelCompleted(){
+        SceneManager.LoadScene(1);
+    }
+}
