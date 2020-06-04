05b891b0eded916e1af2240384ba0937a0adc2b3
Professor Fabio Ramos <fabio.sramos@ceub.edu.br>
2020-06-03 21:05:43 -0300

    01-06-20

---
+++ Assets/Scripts/ParallaxEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxEffect : MonoBehaviour
+{
+    public Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    public float parallaxSpeed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTransform = GameObject.FindGameObjectWithTag("MainCamera").transform;
+        lastCameraPosition = cameraTransform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += deltaMovement*parallaxSpeed;
+        lastCameraPosition = cameraTransform.position;
+    }
+}
